29a79637db6c3a2d8d937e354d6013f63bacabec
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2023-03-29 14:22:36 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
