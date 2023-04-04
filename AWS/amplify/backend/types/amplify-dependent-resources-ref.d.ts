f0ec0837a5a268a41af228dfe4e46a4c99655a69
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2023-04-04 16:29:42 +0200

    AWS login-register

--- AWS/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ AWS/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "connectdevices01177391": {
+    "studyspacemobileappauth1": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "studyspacemobileappauth1PreSignup": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
