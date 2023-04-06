bcd4da1c7b9b9f174c5bfbcb2b1ffc699e58e045
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2023-04-06 16:51:38 +0200

    create model provider

--- AWS/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ AWS/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "connectdevices": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "studyspacemobileappauth1": {
       "AppClientID": "string",
