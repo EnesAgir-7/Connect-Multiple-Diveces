a1541d3fc7cfdcdab6e5f2d6dc23c6e29765015c
Enes Agir <84997756+EnesAgir-7@users.noreply.github.com>
2023-04-11 17:04:09 +0200

    GraphQL Api set up

--- AWS/lib/main.dart
+++ AWS/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_datastore/amplify_datastore_stream_controller.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:new_app1/models/ModelProvider.dart';
 import 'package:new_app1/pages/home.dart';
 
 import 'amplifyconfiguration.dart';
@@ -18,20 +22,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late ModelProvider modelProvider;
+  bool amplifyConfig = false;
   @override
   void initState() {
     super.initState();
+    modelProvider = ModelProvider.instance;
     _configureAmplify();
   }
 
   void _configureAmplify() async {
-    try {
-      await Amplify.addPlugin(AmplifyAuthCognito());
-      await Amplify.configure(amplifyconfig);
-      print('Successfully configured');
-    } on Exception catch (e) {
-      print('Error configuring Amplify: $e');
+    if (!Amplify.isConfigured) {
+      final auth = AmplifyAuthCognito();
+      final dataStore = AmplifyDataStore(modelProvider: ModelProvider.instance);
+      final AmplifyAPI apiPlugin = AmplifyAPI(modelProvider: ModelProvider.instance);
+
+      try {
+        await Amplify.addPlugins([auth, dataStore, apiPlugin]);
+        await Amplify.configure(amplifyconfig);
+        print('------------------------');
+        print('Successfully configured');
+      } on Exception catch (e) {
+        print('Error configuring Amplify: $e');
+      }
     }
+    setState(() {
+      amplifyConfig = true;
+    });
   }
 
   @override
@@ -39,7 +56,7 @@ class _MyAppState extends State<MyApp> {
     return Authenticator(
       child: MaterialApp(
         builder: Authenticator.builder(),
-        home: const HomePage(),
+        home: amplifyConfig ? const HomePage() : Center(child: CircularProgressIndicator()),
       ),
     );
   }
